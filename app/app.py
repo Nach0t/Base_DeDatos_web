0c381eaaa992b87627f03914a0e4996bc97b0109
vicentexte <vquintanillah@correo.uss.cl>
2024-11-28 22:57:51 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ app.secret_key = 'your_secret_key'  # Add a secret key for session management (i
 
 # Configuración de la base de datos
 config = {
-    'host': 'mysql',
+    'host': 'mysql-1',
     'port': '3306',
     'database_name': 'universitydb',
     'user': 'root',
